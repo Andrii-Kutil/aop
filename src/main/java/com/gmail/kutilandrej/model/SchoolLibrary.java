4d0e33a0e4c5bdae6c76a6e2d8d23c73ab60fc73
Andrii Kutil <Andrey_Kutil@service-team.biz>
2021-12-29 18:34:06 +0200

    added tab to sout

--- src/main/java/com/gmail/kutilandrej/model/SchoolLibrary.java
+++ src/main/java/com/gmail/kutilandrej/model/SchoolLibrary.java
@@ -7,24 +7,24 @@ public class SchoolLibrary implements Library {
 
   @Override
   public void getBook(String bookName, String person) {
-    System.out.println("      EXEC SchoolLibrary; getBook(String bookName, String person);");
+    System.out.println("\t\tEXEC SchoolLibrary; getBook(String bookName, String person);");
   }
 
   @Override
   public boolean returnBook(String bookName, String person) throws Exception {
-    System.out.println("      EXEC SchoolLibrary; returnBook(String bookName, String person);");
+    System.out.println("\t\tEXEC SchoolLibrary; returnBook(String bookName, String person);");
     throw new Exception("SchoolLibrary; returnBook(String bookName, String person)");
 
   }
 
   @Override
   public boolean addBook(String bookName) {
-    System.out.println("      EXEC SchoolLibrary; addBook(String);");
+    System.out.println("\t\tEXEC SchoolLibrary; addBook(String);");
     return true;
   }
 
   public void throwMyException() {
-    System.out.println("      EXEC SchoolLibrary; throwException();");
+    System.out.println("\t\tEXEC SchoolLibrary; throwException();");
     throw new NullPointerException();
   }
 }
--- src/main/java/com/gmail/kutilandrej/model/UniversityLibrary.java
+++ src/main/java/com/gmail/kutilandrej/model/UniversityLibrary.java
@@ -7,24 +7,24 @@ public class UniversityLibrary implements Library {
 
   @Override
   public void getBook(String bookName, String person) {
-    System.out.println("      EXEC UniversityLibrary; getBook(String bookName, String person);");
+    System.out.println("\t\tEXEC UniversityLibrary; getBook(String bookName, String person);");
   }
 
   @Override
   public boolean returnBook(String bookName, String person) throws Exception {
-    System.out.println("      EXEC UniversityLibrary; returnBook(String bookName, String person);");
+    System.out.println("\t\tEXEC UniversityLibrary; returnBook(String bookName, String person);");
     throw new Exception("UniversityLibrary; returnBook(String bookName, String person)");
   }
 
   @Override
   public boolean addBook(String bookName) {
-    System.out.println("      EXEC UniversityLibrary; addBook(String);");
+    System.out.println("\t\tEXEC UniversityLibrary; addBook(String);");
     return true;
   }
 
   @Override
   public void throwMyException() {
-    System.out.println("      EXEC UniversityLibrary; throwException();");
+    System.out.println("\t\tEXEC UniversityLibrary; throwException();");
     throw new NullPointerException();
   }
 }
